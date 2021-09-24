edf39a3a9327ad045abfeeb05d993585d8bdc3e3
Gabriel-Belloni <gabriel-belloni@hotmail.com>
2021-09-24 17:00:38 -0300

    exercícios dia 24/09

--- exercicio 24 09/exercicio3.js
+++ exercicio 24 09/exercicio3.js
@@ -3,7 +3,7 @@ var var2 =8;
 var soma =var1+var2;
 
 if(soma % 2 == 0){
-    console.log('par')
+    console.log(soma+' é par')
 } else {
     console.log('impar');
 }
--- exercicio 24 09/exercicio6.js
+++ exercicio 24 09/exercicio6.js
@@ -2,5 +2,5 @@ var x = 15;
 
 for(var i =0; i <= x; i++){
     if(i % 2 !=0)
-    console.log(i) 
+    console.log("os impares entre 0 e "+x+ " são "+i) 
 }
