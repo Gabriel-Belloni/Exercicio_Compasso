f4d2b96ab054f9ecf06e8e856331ac4f3069e16a
Gabriel-Belloni <gabriel-belloni@hotmail.com>
2021-09-24 17:11:20 -0300

    exercícios dia 24/09

--- exercicio 24 09/exercicio6.js
+++ exercicio 24 09/exercicio6.js
@@ -1,6 +1,6 @@
 var x = 15;
-
+console.log("os impares entre 0 e "+x+ " são: ")
 for(var i =0; i <= x; i++){
     if(i % 2 !=0)
-    console.log("os impares entre 0 e "+x+ " são "+i) 
+    console.log(i) 
 }
